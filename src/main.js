fcb43abb69f9a2b751c2a464cd774d0e651a3065
Nikhil <nikhil22322@iiitd.ac.in>
2024-06-23 19:46:16 +0530

    added js file for index.html

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const searchlogo = document.getElementById("enableSearchBox")
+const searchBox = document.getElementById("search-area")
+
+searchlogo.addEventListener("click",()=>{
+    searchBox.style.display="flex";
+})
